6c12e1c28a1c566b23817514dfa7546feef8dbc2
danielstena <32764564+danielstena@users.noreply.github.com>
2018-01-31 18:50:36 +0100

    update engelskt brev

--- js/personligtBrev.js
+++ js/personligtBrev.js
@@ -39,10 +39,10 @@ function eng(){
 
     $("#wrapper").empty()
     var html ="";
-    html += '<div class="textEng"><h1>Hej!</h1><p>Jag studerar webutveckling inom e-handel på medieinstitutet i Göteborg. </p>'
-    html += '<p>Vi kommer under hösten 2018 utföra praktik(LIA) inom passande arbetsområder för utbildningen.</p>'
-    html += '<p>Mitt krav till dig som arbetsgivare är att jag får praktisera och utföra uppgifter för er inom programmering och webdesign. </p>'
-    html += ' <p>Har ni någon passande plats för mig på erat företag?</p>'
+    html += '<div class="textEng"><h1>Hi!</h1><p style="width:">I am studying web development in e-commerce at the media institute in Gothenburg.</p>'
+    html += '<p>In autumn 2018, we will conduct internships (LIA) in the appropriate areas of work for the education.</p>'
+    html += '<p> My requirement for you as an employer is that I can practice and execute tasks for you in programming and web design.</p>'
+    html += ' <p>Do you have any suitable place for me on at your company?</p>'
     html += '<div id="buttons"><button id="back"onclick="backToLanguage()">Språk</button>'
     html += '<a href="index.html"><button id="back">CV</button></a></div></div>'
     $('#textEng').animate( { width: spanWidth }, 3000 );
